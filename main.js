4a9bc0233a28dfdf8e486ff9545c36f17bb6e7c8
Aaron Santiago <me@aaron.work>
2025-05-03 16:49:49 -0400

    setup pretty printing

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-import config from '3lib-config';
-import {execa} from 'execa'
-import * as toml from 'smol-toml';
+import config from "3lib-config";
+import { execa } from "execa";
+import moment from "moment/moment";
+import * as toml from "smol-toml";
 
 config.init();
 let processes = config.get("processes", []);
 
-if (processes.length == 0) console.error("no processes defined. check or create config.toml file.")
+if (processes.length == 0)
+  console.error("no processes defined. check or create config.toml file.");
 
 for (let process of config.get("processes")) {
-  let processConfig = null;
+  let processConfig, processConfigEncoded;
   if (process.loadConfig) {
     processConfig = config.get("configs/" + process.loadConfig, null);
-    if (config == null) console.error("config", process.loadConfig, "is not defined");
-    processConfig = encodeURI(toml.stringify(processConfig));
+    if (processConfig == null)
+      console.error("config", process.loadConfig, "is not defined");
+    processConfigEncoded = encodeURI(toml.stringify(processConfig));
   }
   if (process.tomlConfig) {
     processConfig = process.tomlConfig;
-    processConfig = encodeURI(toml.stringify(processConfig));
+    processConfigEncoded = encodeURI(toml.stringify(processConfig));
   }
-  ; (async () => {
-    console.log("executing", process.exec, processConfig)
-    for await (const result of execa(process.exec, [processConfig])) {
-      console.log(process.name, result);
+  (async () => {
+    console.log("executing", process.exec, processConfig);
+    for await (const result of execa(process.exec, [processConfigEncoded])) {
+      console.log(
+        `${moment().format("YYYY-MM-DD_HH:mm:ss.SS")} | ${process.name}: ${result}`,
+      );
     }
   })();
-
 }
