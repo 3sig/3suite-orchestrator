37ea2ac9c8db2322bdcc0e11bc37d252181ba8e8
Aaron Santiago <me@aaron.work>
2025-05-03 17:03:36 -0400

    setup logging to file, with truncations for easier reading/preventing massive logs due to logs containing file representations

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ import config from "3lib-config";
 import { execa } from "execa";
 import moment from "moment/moment";
 import * as toml from "smol-toml";
+import fs from "fs";
 
 config.init();
 let processes = config.get("processes", []);
 
 if (processes.length == 0)
   console.error("no processes defined. check or create config.toml file.");
+
+let logStream;
+
+if (config.get("writeToFile", false)) {
+  logStream = fs.createWriteStream(config.get("writeToFile"), { flags: 'a' });
+}
 
 for (let process of config.get("processes")) {
   let processConfig, processConfigEncoded;
@@ -24,9 +31,22 @@ for (let process of config.get("processes")) {
   (async () => {
     console.log("executing", process.exec, processConfig);
     for await (const result of execa(process.exec, [processConfigEncoded])) {
-      console.log(
-        `${moment().format("YYYY-MM-DD_HH:mm:ss.SS")} | ${process.name}: ${result}`,
-      );
+
+      let msg = `${moment().format("YYYY-MM-DD_HH:mm:ss.SS")} | ${process.name}: ${result}`;
+      let msgConsole = msg;
+      let msgFile = msg;
+      let maxLineLengthConsole = config.get("truncateLineLengthConsole", 0);
+      if (maxLineLengthConsole > 0 && msg.length > maxLineLengthConsole) {
+        msgConsole = msg.substring(0, maxLineLengthConsole - 3) + "...";
+      }
+      let maxLineLengthFile = config.get("truncateLineLengthFile", 0);
+      if (maxLineLengthFile > 0 && msg.length > maxLineLengthFile) {
+        msgFile = msg.substring(0, maxLineLengthFile - 3) + "...";
+      }
+
+      console.log(msgConsole);
+      if (config.get("truncateInFile", false)) msg = truncated;
+      logStream?.write(msgFile + "\n");
     }
   })();
 }
